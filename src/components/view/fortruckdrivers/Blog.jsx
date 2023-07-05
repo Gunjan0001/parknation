0e47a350c4b120e3b75f2425b4a12c5493d2c9f2
PreetiYdv <preetiyadav5443@gmail.com>
2023-07-05 16:58:16 +0530

    Blog Hero Done

--- src/components/view/fortruckdrivers/Blog.jsx
+++ src/components/view/fortruckdrivers/Blog.jsx
@@ -1,8 +1,14 @@
 import React from "react";
 import BlogHero from "../../ForTruckDrivers/blog/BlogHero";
+import BlogCards from "../../ForTruckDrivers/blog/BlogCards";
 
 const Blog = () => {
-  return <BlogHero />;
+  return (
+    <>
+      <BlogHero />
+      <BlogCards />
+    </>
+  );
 };
 
 export default Blog;
