44d3b802fa62f5a6a6291ec141fb13efe766c6d8
sakshi pareek <sakshipareek505@gmail.com>
2023-07-05 16:07:30 +0530

    blog-details-heading

--- src/components/ForTruckDrivers/blog-detail/Management.jsx
+++ src/components/ForTruckDrivers/blog-detail/Management.jsx
@@ -2,7 +2,14 @@ import React from 'react'
 
 const Management = () => {
   return (
-    <div>Management</div>
+    <section>
+      <div className='container'>
+        <div className='max-w-[873px]'>
+          <h2 className='text-4xl text-black font-bold mb-[17px]'>Excel spreadsheets make it difficult to scale, collaborate, and organize documents, as there is no document management</h2>
+          
+        </div>
+      </div>
+    </section>
   )
 }
 
