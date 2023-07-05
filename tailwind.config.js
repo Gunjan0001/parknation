21b717fcb929a1a52aea70cba43fae7449e5b241
gunjan <gunjandora595@gmail.com>
2023-07-05 15:12:56 +0530

    new

---
+++ tailwind.config.js
@@ -0,0 +1,37 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  important: true,
+  theme: {
+    container: {
+      center: true,
+      padding: {
+        DEFAULT: "1rem",
+        lg: "4.375rem",
+        "2xl": "12.375rem",
+      },
+    },
+    extend: {
+      fontSize: {
+        xs: "12px",
+        sm: "14px",
+        base: "16px",
+        md: "20px",
+        lg: "22px",
+        xl: "24px",
+        "3xl": "32px",
+        "4xl": "40px",
+        "5xl": "54px",
+        "6xl": "56px",
+      },
+
+      colors: {
+        primary: "#EB2022",
+        secondary: "#26324D",
+        black: "#020202",
+        white: "#FAFAFA",
+      },
+    },
+  },
+  plugins: [require("tailwind-scrollbar")],
+};
