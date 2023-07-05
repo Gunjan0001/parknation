47085681621135cc9fbc05571da6f0652cb3189b
PreetiYdv <preetiyadav5443@gmail.com>
2023-07-05 16:06:13 +0530

    Blog Hero

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import BlogHero from "./components/ForTruckDrivers/blog/BlogHero";
 
 function App() {
   return (
     <>
-    
+      <BlogHero />
     </>
   );
 }
--- src/components/ForTruckDrivers/blog/BlogHero.jsx
+++ src/components/ForTruckDrivers/blog/BlogHero.jsx
@@ -1,15 +1,26 @@
 import React from 'react'
+import man_with_truck from "../../../assests/images/png/blog_manWithTruck.png"
 
 const BlogHero = () => {
   return (
     <>
       <section>
         <div className="container">
-          <h2 className=' text-black font-bold text-3xl mb-4'>
+          <h2 className=" text-black font-bold text-3xl mb-4">
             Excel spreadsheets make it difficult to scale, collaborate, and
             organize documents, as there is no document management
           </h2>
-          <p className=' text-black font-normal text-base opacity-60 mb-8'>Published on March 24/2023</p>
+          <p className=" text-black font-normal text-base opacity-60 mb-8">
+            Published on March 24/2023
+          </p>
+          <div className="flex">
+            <div className=" lg:w-1/2">
+              <img src={man_with_truck} alt="man_with_truck" />
+            </div>
+            <div className=" lg:w-1/2">
+              <h4>TLDR - Here's what you need to know:</h4>
+            </div>
+          </div>
         </div>
       </section>
     </>
