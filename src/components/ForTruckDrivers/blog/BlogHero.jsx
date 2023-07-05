ebf7f6fa29c13de542df2f6d26d7033b9c3782cf
PreetiYdv <preetiyadav5443@gmail.com>
2023-07-05 16:29:51 +0530

    blog hero

--- src/components/ForTruckDrivers/blog/BlogHero.jsx
+++ src/components/ForTruckDrivers/blog/BlogHero.jsx
@@ -13,23 +13,29 @@ const BlogHero = () => {
           <p className=" text-black font-normal text-base opacity-60 mb-8">
             Published on March 24/2023
           </p>
-          <div className="flex">
-            <div className=" lg:w-1/2">
-              <img src={man_with_truck} alt="man_with_truck" className='w-full'/>
+          <div className="lg:flex items-start lg:gap-14 md:gap-8">
+            <div className=" lg:w-1/2 w-full">
+              <img
+                src={man_with_truck}
+                alt="man_with_truck"
+                className="w-full"
+              />
             </div>
-            <div className="lg:w-1/2">
-              <h4 className=' text-black text-md font-bold'>TLDR - Here's what you need to know:</h4>
-              <ul>
-                <li className=' mb-4 text-base font-normal opacity-60 text-black'>
+            <div className="lg:w-1/2 w-full lg:mt-0 mt-4">
+              <h4 className=" text-black text-md font-bold">
+                TLDR - Here's what you need to know:
+              </h4>
+              <ul className="list-disc">
+                <li className=" mt-4 text-base font-normal opacity-60 text-black">
                   Excel spreadsheets make it difficult to scale, collaborate,
                   and organize documents, as there is no document management
                 </li>
-                <li className=' mb-4 text-base font-normal opacity-60 text-black'>
+                <li className=" mt-4 text-base font-normal opacity-60 text-black">
                   On-premise TMS systems require physical servers located at
                   your office, which are often costly both to purchase and
                   maintain. You also need to worry about security & privacy
                 </li>
-                <li className=' mb-4 text-base font-normal opacity-60 text-black'>
+                <li className=" mt-4 text-base font-normal opacity-60 text-black">
                   The Rose Rocket TMS is cloud-based, secure & private, helps
                   streamline collaboration, and future-proof your business.
                   Learn more about cloud-based TMS software.
